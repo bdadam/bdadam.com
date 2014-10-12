3965490f52f389f34ccf567caae52dc8cf208784
bdadam <bdadam@gmail.com>
2014-10-12 13:42:12 +0200

    generating font.css from woff files

---
+++ dist/static/fonts/gencss.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+var util = require('util');
+
+var files = [{
+	filename: 'Source Sans Pro Regular.woff',
+	name: 'Source Sans Pro',
+	'font-weight': '400',
+	'font-style': 'normal'
+}, {
+	filename: 'Source Sans Pro Semibold.woff',
+	name: 'Source Sans Pro',
+	'font-weight': '600',
+	'font-style': 'normal'
+}];
+
+var fontCssFile = '../source-sans-pro.woff.css';
+
+fs.writeFileSync(fontCssFile, '');
+
+files.forEach(function(x) {
+	var b64 = fs.readFileSync(x.filename).toString('base64');
+	var format = '@font-face{font-family:%s;src:local("%s"),url(data:application/x-font-woff;charset=utf-8;base64,%s) format("woff");font-weight:%s;font-style:%s}';
+	var css = util.format(format, x.name, x.name, b64, x['font-weight'], x['font-style']);
+
+	fs.appendFileSync(fontCssFile, css);
+});
