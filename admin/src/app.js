5debab7ed7b8da3d0556c3adda79db06737199f9
bdadam <me@bdadam.com>
2018-05-16 01:18:15 +0200

    fixed article-asset path for admin

--- admin/src/app.js
+++ admin/src/app.js
@@ -1,8 +1,10 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 
 const gqlRouter = require('./gql');
 const nuxtRenderer = require('./nuxt');
 
+app.use('/static/article-assets/', express.static('../dist/static/article-assets'));
 app.use('/gql', gqlRouter);
 app.use(nuxtRenderer);
 
