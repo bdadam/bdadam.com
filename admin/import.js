c79e3087a1922ed4a53f13c200d94d53e8e5305e
bdadam <me@bdadam.com>
2018-05-15 23:51:13 +0200

    importer for old articles

--- admin/import.js
+++ admin/import.js
@@ -2,6 +2,8 @@ const fs = require('fs-extra');
 const path = require('path');
 const glob = require('glob');
 
+const dateFormat = require('dateformat');
+
 const { promisify } = require('util');
 
 const yfm = require('yaml-front-matter');
@@ -32,13 +34,39 @@ const importPost = async post => {
     }, { upsert: true });
 };
 
+const matter = require('gray-matter');
+const writePost = (post, id) => {
+    const content = `${(post.abstract || '').trim()}\n\n<!-- readmore -->\n\n${(post.content || '').trim()}`.replace(/\r/g, '');
+    const slug = post.filename.replace(/\.md$/, '');
+    const date = new Date(post.date);
+
+    const fm = matter.stringify(`\n${content}`, {
+        title: post.title,
+        description: post.description || '',
+        date: new Date(post.date),
+        tags: post.tags || []
+    });
+
+    // const datePrefix = dateFormat(post.date, 'yyyy-mm-dd-HH-MM-ss');
+    // fs.writeFileSync(`content/blog/${datePrefix}-${slug}.md`, fm);
+    fs.writeFileSync(`content/blog/${id.toString().padStart(4, '0')}-${slug}.md`, fm);
+};
+
 (async () => {
-    await db.posts.ensureIndex({ fieldName: 'slug', unique: true });
+    // await db.posts.ensureIndex({ fieldName: 'slug', unique: true });
 
     const postFiles = await findPosts();
     const posts = postFiles.map(file => ({ filename: path.basename(file), ...yfm.loadFront(fs.readFileSync(file), { contentKeyName: 'content' }) }));
+    posts.sort((p1, p2) => {
+        return new Date(p1.date) - new Date(p2.date);
+    });
 
-    await Promise.all(posts.map(importPost));
+    fs.ensureDirSync('content/blog');
 
-    db.posts.persistence.compactDatafile();
+    let i = 1;
+    posts.forEach(post => writePost(post, i++));
+
+    // await Promise.all(posts.map(importPost));
+
+    // db.posts.persistence.compactDatafile();
 })();
