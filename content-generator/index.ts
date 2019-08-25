73646a49bb583ec7c46caabac4b663ddfec3a985
bdadam <me@bdadam.com>
2019-08-26 01:05:15 +0200

    Content generator first steps

--- content-generator/index.ts
+++ content-generator/index.ts
@@ -6,22 +6,35 @@ import glob from 'glob-promise';
 import matter from 'gray-matter';
 
 const generate = async () => {
-    const articles = await glob('content/blog/**/*.md');
-    const x = articles.map(f => ({ content: fs.readFileSync(f, 'utf-8'), filename: f }));
-    const y = x.map(c => {
+    console.log('Generating...');
+
+    const articlesMdFiles = await glob('content/blog/**/*.md');
+    const x = articlesMdFiles.map(f => ({ content: fs.readFileSync(f, 'utf-8'), filename: f }));
+    const articles = x.map(c => {
         const qwe = matter(c.content, { excerpt: false });
+
+        // if (qwe.data.published === false) {
+        //     console.log(qwe);
+        // }
+
         return {
-            ...qwe,
-            filename: c.filename,
+            // ...qwe,
+            // filename: c.filename,
+            published: qwe.data.published !== false,
+            title: qwe.data.title,
             content: marked(qwe.content),
+            abstract: qwe.data.abstract as string,
+            tags: qwe.data.tags as string[],
+            date: qwe.data.date as Date,
             slug: path.basename(c.filename, '.md'),
         };
     });
-    console.log(y);
+
+    const publishedArticles = articles.filter(a => a.published);
+    // const notPublishedArticles = articles.filter(a => !a.published);
+    // console.log(notPublishedArticles);
 
     // console.log(articles);
-
-    console.log('Generate');
 };
 
 generate();
