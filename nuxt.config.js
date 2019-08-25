616eb80f5b885948608eec6e71df52dda394980a
bdadam <me@bdadam.com>
2019-08-26 00:04:09 +0200

    Using system font

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,10 +2,12 @@ export default {
     head: {
         meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
         link: [
-            {
-                rel: 'stylesheet',
-                href: 'https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap&subset=latin-ext',
-            },
+            // {
+            //     rel: 'stylesheet',
+            //     // href: 'https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=optional&subset=latin-ext',
+            //     href: 'https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap&subset=latin-ext',
+            //     // href: 'https://fonts.googleapis.com/css?family=Quicksand:400,700&display=swap&subset=latin-ext',
+            // },
         ],
     },
     css: ['~/styles/global.less', '@fortawesome/fontawesome-svg-core/styles.css'],
