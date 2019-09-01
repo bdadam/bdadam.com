82bdea7f48683840e38f6eeeb58d69693078edb1
bdadam <me@bdadam.com>
2019-09-02 01:24:21 +0200

    Better styling for popular articles

--- src/components/PopularArticles.svelte
+++ src/components/PopularArticles.svelte
@@ -1,36 +1,37 @@
 <style lang="less">
     .most-popular-box {
         background-color: #f1f1f1;
-        padding-top: 12px;
-        padding-bottom: 12px;
         display: flex;
         flex-direction: row;
         flex-wrap: nowrap;
         padding-left: 24px;
         padding-right: 24px;
         overflow-x: auto;
+        align-items: center;
 
         @media (min-width: 1148px) {
             padding-left: calc((100% - 1100px) / 2);
             padding-right: calc((100% - 1100px) / 2);
         }
 
-        span,
         a {
-            display: inline-block;
-            margin-right: 12px;
+            display: block;
             flex-shrink: 0;
+            padding-top: 12px;
+            padding-bottom: 12px;
         }
 
-        span {
-            font-variant: small-caps;
+        a {
+            margin: 0 6px;
         }
     }
 </style>
 
 <div class="most-popular-box">
-    <span>Popular</span>
+    Popular:
     <a href="/blog/better-webfont-loading-with-localstorage-and-woff2/">Webfont loading</a>
+    &middot;
     <a href="/blog/finding-a-random-document-in-mongodb/">Mongodb random element</a>
+    &middot;
     <a href="/blog/automatically-adapting-the-height-textarea/">Textarea auto height</a>
 </div>
