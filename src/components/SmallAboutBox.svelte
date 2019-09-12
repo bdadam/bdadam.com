5d734f9f70cdaa001ea72532f9b9c993cf4c7589
bdadam <me@bdadam.com>
2019-09-13 01:12:49 +0200

    Small about box

---
+++ src/components/SmallAboutBox.svelte
@@ -0,0 +1,38 @@
+<style lang="less">
+    .about {
+        background-color: #224477;
+        background-color: #135;
+        background-color: #247;
+        background-color: #f1f1f1;
+        border-top: 1px solid #cdcdcd;
+        /* color: #fff; */
+
+        padding: 24px;
+        padding-left: calc((100% - 1100px) / 2);
+        padding-right: calc((100% - 1100px) / 2);
+
+        a {
+            /* color: #fff; */
+            /* text-decoration: underline; */
+        }
+    }
+</style>
+
+<div class="about">
+    <p>Hello and welcome to my blog. I'm Adam Beres-Deak. I'm a software engineer working in Munich, Germany.</p>
+    <p>
+        I have been professionally working on web projects since 2005.
+        <a href="/about/">More about me</a>
+        <!-- On this site I write about
+        <a href="/tags/code/">code,</a>
+        <a href="/tags/architecture/">architecture,</a>
+        <a href="/tags/cloud/">cloud,</a>
+        <a href="/tags/shell/">shell</a>
+        and everything else I learn and want to share with others. -->
+    </p>
+    <!-- <p>
+        If you like my work, you can
+        <a href="/about/">hire me</a>
+        .
+    </p> -->
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,53 +10,20 @@
 <script>
     import ArticleItemList from '../components/ArticleItemList.svelte';
     import PopularArticles from '../components/PopularArticles.svelte';
+    import SmallAboutBox from '../components/SmallAboutBox.svelte';
+
     export let data;
 </script>
 
-<style lang="less">
-    .about {
-        background-color: #224477;
-        background-color: #135;
-        background-color: #247;
-        color: #fff;
-        padding: 24px;
-        padding-left: calc((100% - 1100px) / 2);
-        padding-right: calc((100% - 1100px) / 2);
-
-        a {
-            color: #fff;
-            text-decoration: underline;
-        }
-    }
-</style>
-
 <svelte:head>
     <title>Adam Beres-Deak</title>
-    <meta
-        name="description"
-        content="My devblog about web development, cloud architecture, JavaScript, NodeJS, CSS, less" />
+    <meta name="description" content="My devblog about Web Development, Cloud Architecture, JavaScript, Node.js, CSS" />
 </svelte:head>
 
 <PopularArticles />
 
-<div class="about">
-    <p>Hello and welcome to my blog. I'm Adam Beres-Deak. I'm a software engineer working in Munich, Germany.</p>
-    <p>
-        I have been professionally working on web projects since 2005.
-        <a href="/about/">More about me</a>
-        <!-- On this site I write about
-        <a href="/tags/code/">code,</a>
-        <a href="/tags/architecture/">architecture,</a>
-        <a href="/tags/cloud/">cloud,</a>
-        <a href="/tags/shell/">shell</a>
-        and everything else I learn and want to share with others. -->
-    </p>
-    <!-- <p>
-        If you like my work, you can
-        <a href="/about/">hire me</a>
-        .
-    </p> -->
-</div>
+<SmallAboutBox />
+
 <div class="page-container">
 
     <h2>Latest articles</h2>
