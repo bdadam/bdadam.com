4764cc36f686fe24409969db2a233ba665871f3f
bdadam <me@bdadam.com>
2019-09-18 10:18:53 +0200

    Prism js is imported dynamically

---
+++ src/helpers/prism.js
@@ -0,0 +1,7 @@
+import Prism from 'prismjs';
+import 'prismjs/components/prism-markup-templating';
+import 'prismjs/components/prism-handlebars';
+import 'prismjs/components/prism-nginx';
+import 'prismjs/components/prism-csharp';
+
+export default Prism;
--- src/routes/blog/[slug]/index.svelte
+++ src/routes/blog/[slug]/index.svelte
@@ -13,12 +13,6 @@
     import ArticleComments from '../../../components/ArticleComments.svelte';
     import { onMount } from 'svelte';
 
-    import Prism from 'prismjs';
-    import 'prismjs/components/prism-markup-templating';
-    import 'prismjs/components/prism-handlebars';
-    import 'prismjs/components/prism-nginx';
-    import 'prismjs/components/prism-csharp';
-
     export let url;
     export let slug;
     export let title;
@@ -61,7 +55,7 @@
     }
 
     onMount(() => {
-        Prism.highlightAll();
+        import('../../../helpers/prism.js').then(Prism => Prism.default.highlightAll());
     });
 </script>
 
