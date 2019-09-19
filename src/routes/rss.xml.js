258ea7e2055b7b3ef436911fa41ef13c7722b9fb
bdadam <me@bdadam.com>
2019-09-20 01:30:32 +0200

    First draft for rss support

--- src/routes/rss.xml.js
+++ src/routes/rss.xml.js
@@ -1,7 +1,45 @@
+import RSS from 'rss';
+
 export async function get(req, res) {
     res.set('Content-Type', 'application/rss+xml');
 
-    res.end(`<?xml version="1.0" encoding="utf-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <url><loc>https://bdadam.com/</loc></url>
-    </urlset>`);
+    const feed = new RSS({
+        title: 'Adam Beres-Deak',
+        description: 'Blog',
+        site_url: 'https://bdadam.com/',
+        feed_url: 'https://feeds.feedburner.com/bdadamcom',
+        image_url: '', // TODO
+    });
+
+    // TODO
+    feed.item({
+        title: 'TODO',
+        description: '<p>TODO</p>',
+        url: 'TODO',
+        author: 'Adam Beres-Deak <me@bdadam.com>',
+    });
+
+    res.send(feed.xml({ indent: true }));
+
+    // res.end(`<?xml version="1.0" encoding="UTF-8" ?>
+    // <?xml-stylesheet type="text/css" href="http://bdadam.com/static/main.css" ?>
+    // <rss version="2.0">
+    //     <channel>
+    //         <title>Adam Beres-Deak</title>
+    //         <link>http://bdadam.com/</link>
+    //         <description>Blog</description>
+    //         {{#latestPosts pages}}
+    //         <item>
+    //             <title>{{data.title}}</title>
+    //             <link>http://bdadam.com/{{ relativeLink }}</link>
+    //             <description>
+    //                 <![CDATA[
+    //                 {{#markdown}}{{{data.abstract}}}{{/markdown}}
+    //                 {{#markdown}}{{{page}}}{{/markdown}}
+    //                 ]]>
+    //             </description>
+    //         </item>
+    //         {{/latestPosts}}
+    //     </channel>
+    // </rss>`);
 }
