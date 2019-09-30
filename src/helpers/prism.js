b691feeb901a38ac9e5aa039445daedd6617cb8c
Adam <me@bdadam.com>
2019-09-30 23:58:35 +0200

    Code highlighting for bash/shell/sh

--- src/helpers/prism.js
+++ src/helpers/prism.js
@@ -3,5 +3,8 @@ import 'prismjs/components/prism-markup-templating';
 import 'prismjs/components/prism-handlebars';
 import 'prismjs/components/prism-nginx';
 import 'prismjs/components/prism-csharp';
+import 'prismjs/components/prism-bash';
+
+Prism.languages.sh = Prism.languages.bash;
 
 export default Prism;
