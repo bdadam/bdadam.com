986de1edde2354527e167f8135890b43111d2876
Adam <me@bdadam.com>
2020-05-03 23:35:18 +0200

    Using icons from https://simpleicons.org/

--- src/pages/about.html.tsx
+++ src/pages/about.html.tsx
@@ -1,5 +1,7 @@
 import SiteHeader from '../components/SiteHeader';
 import SiteFooter from '../components/SiteFooter';
+import Link from 'next/link';
+import { GitHub, Twitter, Linkedin, Xing, Email } from 'src/components/Icons';
 
 const AboutPage: React.FC<{}> = () => {
     return (
@@ -7,6 +9,49 @@ const AboutPage: React.FC<{}> = () => {
             <SiteHeader />
             <div className="flex-grow">
                 <h1>Hello</h1>
+
+                <Link href="/">
+                    <a className="hover:underline mb-1">Read my articles</a>
+                </Link>
+                <a href="mailto:me@bdadam.com" className="hover:underline flex items-center mb-1" title="E-Mail">
+                    <Email width={32} height={32} className="mr-1 text-gray-700" />
+                    Send me an e-mail
+                </a>
+                <a
+                    href="https://github.com/bdadam"
+                    className="hover:underline flex items-center mb-1"
+                    rel="external"
+                    title="GitHub"
+                >
+                    <GitHub width={32} height={32} className="mr-1" /> See my code on GitHub
+                </a>
+                <a
+                    href="https://twitter.com/bdadamm"
+                    className="hover:underline flex items-center mb-1"
+                    rel="external"
+                    title="Twitter"
+                >
+                    <Twitter width={32} height={32} className="mr-1" />
+                    Follow me on Twitter
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/bdadam/"
+                    className="hover:underline flex items-center mb-1"
+                    rel="external"
+                    title="LinkedIn"
+                >
+                    <Linkedin width={32} height={32} className="mr-1" />
+                    Say hi on LinkedIn
+                </a>
+                <a
+                    href="https://www.xing.com/profile/Adam_BeresDeak"
+                    className="hover:underline flex items-center mb-1"
+                    rel="external"
+                    title="Xing"
+                >
+                    <Xing width={32} height={32} className="mr-1" />
+                    Finde mich auf Xing
+                </a>
             </div>
             <SiteFooter />
         </div>
