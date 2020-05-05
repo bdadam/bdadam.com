e7547dacd622ebfd290027062765f878be2df5cf
Adam <me@bdadam.com>
2020-05-05 23:55:46 +0200

    Top articles component

---
+++ src/components/TopArticles.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+
+type Props = {
+    articles: Array<{
+        title: string;
+        url: string;
+        intro: string;
+    }>;
+};
+
+const TopArticles: React.FC<Props> = ({ articles }) => {
+    return (
+        <ul>
+            {articles.map((article) => {
+                return (
+                    <li className="mb-6">
+                        <Link href="/blog/[slug]" as={article.url}>
+                            <a className="block mb-1">
+                                <p className="font-bold hover:underline text-gray-800">{article.title}</p>
+                            </a>
+                        </Link>
+                        <p className="mb-1 text-sm text-gray-700" dangerouslySetInnerHTML={{ __html: article.intro }} />
+                        <Link href="/blog/[slug]" as={article.url}>
+                            <a className="text-purple-700 hover:underline">Read more</a>
+                        </Link>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+};
+
+export default TopArticles;
