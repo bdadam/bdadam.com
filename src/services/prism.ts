9d7a798eec11d93fa415e13bc984505b910e4609
Adam <me@bdadam.com>
2020-05-14 17:40:45 +0200

    Prism: ts support + new theme

--- src/services/prism.ts
+++ src/services/prism.ts
@@ -5,6 +5,8 @@ import 'prismjs/components/prism-handlebars';
 import 'prismjs/components/prism-nginx';
 import 'prismjs/components/prism-csharp';
 import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-typescript';
+// import 'prismjs/plugins/line-numbers/prism-line-numbers';
 
 Prism.languages.sh = Prism.languages.bash;
 
