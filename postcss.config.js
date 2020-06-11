9d83fee77d4caacdb77afa91e90c22540350f9c9
Adam <me@bdadam.com>
2020-06-11 16:22:26 +0200

    Tailwind + postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,17 @@
-const purgecss = [
-    '@fullhuman/postcss-purgecss',
-    {
-        content: ['./src/components/**/*.tsx', '.src//pages/**/*.tsx'],
-        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-        whitelist: ['html', 'body'],
-    },
-];
+// const purgecss = [
+//     '@fullhuman/postcss-purgecss',
+//     {
+//         content: ['./src/components/**/*.tsx', '.src//pages/**/*.tsx'],
+//         defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+//         whitelist: ['html', 'body'],
+//     },
+// ];
 
 module.exports = {
     plugins: [
         'postcss-import',
         'tailwindcss',
         'autoprefixer',
-        ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+        // ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
     ],
 };
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    purge: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
+    theme: {},
+    variants: {},
+    plugins: [],
+};
