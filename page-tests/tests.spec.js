4d70077fca8616784b7271f05146629ede5b2d83
bdadam <me@bdadam.com>
2023-03-18 11:58:04 +0100

    Fixed tests, improved site

--- page-tests/tests.spec.js
+++ page-tests/tests.spec.js
@@ -5,8 +5,8 @@ import glob from 'glob';
 
 import { describe, expect, it } from 'vitest';
 
-import cheerio from 'cheerio';
-import { readFileSync } from 'fs';
+import * as cheerio from 'cheerio';
+import { readFileSync, existsSync } from 'fs';
 
 describe('Site Checks', async () => {
     const x = new URL('', import.meta.url);
@@ -16,16 +16,26 @@ describe('Site Checks', async () => {
         ignore: ['**/og/**', '**/article-assets/**', '**/demo/**', '**/samples/**', '**/admin/**'],
     });
 
-    htmlFiles.forEach((file) =>
-        describe(`${file}`, () => {
-            const content = readFileSync(path.resolve('../dist', file), 'utf-8');
-            const $ = cheerio.load(content);
+    it('has Home and About page', () => {
+        expect(htmlFiles).toContain('index.html');
+        expect(htmlFiles).toContain('about.html');
+    });
 
+    htmlFiles.forEach((file) => {
+        const content = readFileSync(path.resolve('../dist', file), 'utf-8');
+        const $ = cheerio.load(content);
+
+        if ($('meta[name=robots]').attr('content')?.includes('noindex')) {
+            console.log(`Article ignored due to noindex: ${file}`);
+            return;
+        }
+
+        describe(`${file}`, () => {
             it('has proper link[rel=canonical]', () => {
                 const canonical = $('link[rel=canonical]').attr('href');
 
                 expect(canonical, 'Canonical').toSatisfy((c) => {
-                    return c.startsWith('https://bdadam.com/') && c.endsWith(file);
+                    return c.startsWith('https://bdadam.com/') && c.endsWith(file.replace('index.html', ''));
                 });
             });
 
@@ -39,7 +49,6 @@ describe('Site Checks', async () => {
 
             it('has proper meta[name=description]', () => {
                 const description = $('html > head > meta[name=description]').attr('content')?.trim();
-                console.log({ description });
 
                 expect(description).toSatisfy((d) => {
                     // TODO: Fail when description too long
@@ -63,18 +72,21 @@ describe('Site Checks', async () => {
             });
 
             it('has an open graph image', () => {
-                const image = $('html head meta[property="og:image"]').attr('content');
+                // const image = $('html head meta[property="og:image"]').attr('content');
+                const image = /** @type {string} */ ($('html head meta[property="og:image"]').attr('content'));
                 const width = $('html head meta[property="og:image:width"]').attr('content');
                 const height = $('html head meta[property="og:image:height"]').attr('content');
 
                 expect(image).toBeDefined();
+                expect(image).toBeTypeOf('string');
                 // TODO: check for image url to start with https://bdadam.com/og/
                 expect(image).toMatch(/\/og\/.+\.png/);
                 expect(parseInt(width ?? '')).toEqual(1200);
                 expect(parseInt(height ?? '')).toEqual(630);
+                expect(existsSync(path.resolve('../dist', image.replace('/og', 'og')))).toEqual(true);
             });
-        })
-    );
+        });
+    });
 
     describe('Sitemap Index', async () => {
         const sitemapIndexXml = readFileSync('../dist/sitemap-index.xml', 'utf-8');
@@ -136,6 +148,50 @@ describe('Site Checks', async () => {
 
         it('has link to sitemap xml', () => {
             expect(content).toContain('Sitemap: https://bdadam.com/sitemap-index.xml');
+        });
+    });
+
+    describe('Blog migration 2023', () => {
+        it('All articles are migrated', () => {
+            const oldArticles = [
+                'blog/displaying-icons-with-custom-elements.html',
+                'blog/plain-javascript-event-delegation.html',
+                'blog/first-year-of-blogging.html',
+                'blog/better-webfont-loading-with-localstorage-and-woff2.html',
+                'blog/video-douglas-crockford-about-the-new-good-parts.html',
+                'blog/automatically-adapting-the-height-textarea.html',
+                'blog/loading-webfonts-with-high-performance.html',
+                'blog/optimistic-page-loading-with-instantclick-io.html',
+                'blog/generating-sound-effects-with-client-side-javascript.html',
+                'blog/static-site-generation-boilerplate.html',
+                'blog/automatically-loading-grunt-tasks-with-matchdep.html',
+                'blog/simple-usability-trick-for-google-maps.html',
+                'blog/switching-background-color-with-gimp.html',
+                'blog/panning-and-scrolling-background-images-using-the-canvas-element.html',
+                'blog/finding-a-random-document-in-mongodb.html',
+                'blog/tracking-clicks-with-google-analytics.html',
+                'blog/comparison-helper-for-handlebars.html',
+                'blog/strange-error-when-installing-npm-package-globally-on-ubuntu.html',
+                'blog/defining-properties-in-javascript.html',
+                'blog/playing-mario-in-the-browser.html',
+                'blog/building-desktop-apps-with-node-js-and-web-technologies.html',
+                'blog/how-to-redirect-www-to-naked-domain-and-vice-versa-with-nginx.html',
+                'blog/how-to-register-a-bower-package.html',
+                'blog/demistifying-angularjs-dependency-injection.html',
+                'blog/hosting-static-web-pages-and-assets-with-google-drive.html',
+                'blog/strong-caching-with-nginx.html',
+                'blog/error-handling-in-javascript.html',
+                'blog/adding-tab-support-to-textareas.html',
+                'blog/serve-a-practical-command-line-webserver.html',
+                'blog/fat-arrows-for-javascript.html',
+                'blog/why-i-chose-a-statically-generated-website.html',
+                'blog/a-simple-pubsub-module-in-javascript.html',
+                'blog/wow-i-started-a-blog.html',
+            ];
+
+            oldArticles.forEach((f) => {
+                expect(htmlFiles).toContain(f);
+            });
         });
     });
 });
