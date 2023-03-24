7a29d787eb0c2e4a6fa58347e560be2e0100858f
bdadam <me@bdadam.com>
2023-03-24 20:45:06 +0100

    Aded images to sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -11,16 +11,18 @@ export const get: APIRoute = async function get() {
                 url: `https://bdadam.com/blog/${post.slug}.html`,
                 title: post.data.title,
                 formattedPublicationDate: new Date(post.data.date).toISOString().substring(0, 10),
+                image: `https://bdadam.com/og/blog/${post.slug}.png`,
             };
         });
 
     const articleUrls: string[] = sitemapData.map((item) => {
         if (new Date(item.formattedPublicationDate).getTime() < Date.now() - 3 * 24 * 60 * 60 * 1000) {
-            return `<url><loc>${item.url}</loc></url>`;
+            return `<url><loc>${item.url}</loc><image:image><image:loc>${item.image}</image:loc></image:image></url>`;
         }
 
         return `<url>
 <loc>${item.url}</loc>
+<image:image><image:loc>${item.image}</image:loc></image:image>
 <news:news>
 <news:publication>
 <news:name>Adam Beres-Deak</news:name>
@@ -34,9 +36,9 @@ export const get: APIRoute = async function get() {
 
     const sitempLines = [
         '<?xml version="1.0" encoding="utf-8"?>',
-        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">',
-        '<url><loc>https://bdadam.com/</loc></url>',
-        '<url><loc>https://bdadam.com/about.html</loc></url>',
+        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">',
+        '<url><loc>https://bdadam.com/</loc><image:image><image:loc>https://bdadam.com/og/home.png</image:loc></image:image></url>',
+        '<url><loc>https://bdadam.com/about.html</loc><image:image><image:loc>https://bdadam.com/og/home.png</image:loc></image:image></url>',
         ...articleUrls,
         '</urlset>',
     ];
