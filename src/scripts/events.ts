e6b8b3a78ad5101f410846503cfa44876abc81fa
bdadam <me@bdadam.com>
2023-03-30 22:59:14 +0200

    Changed events structure

--- src/scripts/events.ts
+++ src/scripts/events.ts
@@ -2,33 +2,37 @@ import { getSession } from './session';
 
 type Event = {
     /** Pageview */
-    pv: 1;
+    pv: '1';
 
     /** Session start */
-    st: 0 | 1;
+    st: '0' | '1';
 
     /** document.title */
-    ti: string;
+    dt: string;
 
     /** window.location.href */
-    lo: string;
+    dl: string;
 
     /** document.referrer */
-    re: string;
+    dr: string;
 
     /** screen resolution */
     sr: string;
+
+    /** navigator.language */
+    ul: string;
 };
 
 export function pageview() {
     const [sid, isNewSession] = getSession();
     const event: Event = {
-        pv: 1,
-        st: isNewSession ? 1 : 0,
-        ti: document.title,
-        lo: location.href,
-        re: document.referrer,
+        pv: '1',
+        st: isNewSession ? '1' : '0',
+        dt: document.title,
+        dl: location.href,
+        dr: document.referrer,
         sr: window.screen ? `${window.screen.width}x${window.screen.height}` : '',
+        ul: navigator.language.toLowerCase(),
     };
 
     sendEvent(sid, event);
@@ -37,9 +41,14 @@ export function pageview() {
 function sendEvent(sid: string, event: Event) {
     requestIdleCallback(() => {
         if (location.href.startsWith('https://')) {
-            navigator.sendBeacon(`/flamingo/hello`, JSON.stringify({ s: sid, e: [event] }));
+            const query = new URLSearchParams({
+                sid,
+                ...event,
+            });
+
+            navigator.sendBeacon('/flamingo/hello?' + query.toString());
         } else {
-            console.log({ s: sid, e: [event] });
+            console.log({ sid, event });
         }
     });
 }
